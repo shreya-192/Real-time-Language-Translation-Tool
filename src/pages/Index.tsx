765a460d6d0bdbd39f52eca4f6cf1ab37e325e88
Shreya Patel <23dit052@charusat.edu.in>
2025-05-31 20:33:02 +0530

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TranslationApp from '../components/TranslationApp';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
+      <TranslationApp />
+    </div>
+  );
+};
+
+export default Index;
